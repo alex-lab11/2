e94fc247d7d6e1dd7dfea467eddfc5f398905636
alex-lab11 <62796829+alex-lab11@users.noreply.github.com>
2020-04-10 21:41:58 +0400

    Update book.h

--- book.h
+++ book.h
@@ -15,25 +15,24 @@ private:
 
 public:
 
-    book();
-    book( char *name ,  char *author , int pagesCount);
-    book(book &);
-    void print();
+    book(const char * const name , const char * const author , const int pagesCount);
+    book(const book &);
+    void print() const;
 
-    void setName( char* );
-    void setAuthor( char* );
-    void setPagesCount(int);
+    void setName(const char* const );
+    void setAuthor(const char* const);
+    void setPagesCount(const int);
 
-    char *getName();
-    char *getAuthor();
-    int getPagesCount();
-    book& operator = ( const book &otherbook);
-    bool operator == (const book& otherbook);
-    bool operator != (const book& otherbook);
-    bool operator <= (const book& otherbook);
-    bool operator < (const book& otherbook);
-    bool operator > (const book& otherbook);
-    bool operator >= (const book& otherbook);
+    char *getName() const;
+    char *getAuthor() const;
+    int getPagesCount() const;
+    book& operator = (const book &otherbook);
+    bool operator == (const book& otherbook) const;
+    bool operator != (const book& otherbook) const;
+    bool operator <= (const book& otherbook) const;
+    bool operator < (const book& otherbook) const;
+    bool operator > (const book& otherbook) const;
+    bool operator >= (const book& otherbook) const;
     friend ostream& operator << (ostream &out, const book &book);
     friend istream& operator >> (istream &in, book &book);
     ~book();
